4711b57a7923c0638e7ee8d8c222f138cd936e42
Pollyana <pollyana.medeirosk@gmail.com>
2018-11-17 20:58:24 -0200

    adiconando escolha de assentos

--- main.c
+++ main.c
@@ -8,20 +8,20 @@
 	
 	//**** definiçao de boleana
 	
-	typedef int bool;
+	typedef int bool; //DEFINIÇÃO TIPO VARIAVEL BOOLEANA
 	#define verdadeira 1
 	#define falso 0
 	
-	//============ STRUCT( definir tipo de variavel ============
+	//============ STRUCT( definir tipo de variavel (TIPO PARTICIPANTE)============
 	
 	typedef struct{
-			char nome[30], email [15],tipo_participante, escolha_de_assento[5];
-		    int cpf;
+			char nome[30], email [15], escolha_de_assento[5];
+		    int cpf, tipo_participante;
 			float data , horas;
 			bool cadeirante;
 	}Participante;
 	
-	//***Variaveis globais
+	//***Variaveis globais ( PODEM SER ACESSADAS EM QUALQUER LUGAR DO ARQUIVO )
 	
 	Participante participante;
 	int assento_professor = 20;
@@ -30,14 +30,16 @@
 	char assentos [10][10]={{" "}};
 	
 	//============ CHAMADA DE FUNÇÕES ============
-	
+    //( CHAMAR EM OUTRAS FUNCOES)	
 	void cadastro();
 	void sorteio();
 	void menu();
 	void mostrarFileira(char assentos[10][10]);
-	void verificar_vagas();
+    void selecionar_assento();
+	//	void verificar_vagas();
 	
 	//============ Função Principal ============
+	// (1 FUNÇÃO QUE RODA)
 	int main(){
 		
 		
@@ -52,20 +54,12 @@
 			printf("\tHora: %d - Min %d", tempo.wHour, tempo.wMinute); //imprime hora e minuto
 		
 		menu();
-		
-		
-		
-		
-		  
-//	    5% cadeirantes, 20% destinados a professores e convidados
-	     
 	
-		
 		return 0;
 	}
 	//============ Funções ============
 	
-	// abre o menu de opçoes;
+	//*** abre o menu de opçoes;
 	void menu(){
 		int escolha;
 		printf("\nDigite opcao desejada de 1 a 2:\n");
@@ -85,6 +79,7 @@
 		}
 	}
 	
+// APRESENTAR FILEIRA NA TELA
 	void mostrarFileira(char assentos[10][10]){
 		char fileira_alfa; 
 		int coluna;
@@ -125,8 +120,9 @@
 	
 		printf("\nDigite 1 professor/convidado, Digite 2 aluno");
 		fflush(stdin);
-		scanf("%c",&participante.tipo_participante);
-		
+		scanf("%d",&participante.tipo_participante);
+			
+			
 		char escolhaPCD;
 		printf("\n Voce e portador de necessidades especiais?[s/n]\n");
 		fflush(stdin);
@@ -151,42 +147,28 @@
 //		printf("Depois da funcao mostrarFileira\n\n");
 	
 		
+		selecionar_assento();
 		
-		
-		printf("\nSelecione o assento. Exemplo:'A 2'\n"); // 
-		fflush(stdin);
-		scanf("%[^\n]s",participante.escolha_de_assento);
-	//	printf("\nescolha_de_assento: %s", escolha_de_assento);
-	    
-		int escolha_fileira = participante.escolha_de_assento[0] - 65;// pegado a escolha da fileira. 65 na tabela ascii = A
-		printf("escolha de fileira: %d\n", escolha_fileira);
-	    fflush(stdin);
-		
-		int escolha_coluna;
-		sscanf(participante.escolha_de_assento, "%*[^0-9]%d", &escolha_coluna); // pegando somente o inteiro do conteúdo do endereço da variavel de escolha de assento
-		escolha_coluna --; 
-//		printf("escolha de coluna: %d\n", escolha_coluna); 
-		
-		assentos[ escolha_fileira][escolha_coluna] = 'x';
-//		printf ("assento marcado: %c\n" );
 		system ("cls");
 		mostrarFileira(assentos);
 		
-	    if (participante.cadeirante == verdadeira){
-	    	assento_cadeirante --;
-	    	printf("assentos restantes cadeirantes %d\n", assento_cadeirante);
+		//VERIFICANDO SE E CADEIRANTE
+	    
+		if (participante.cadeirante == verdadeira){ 
+	    	assento_cadeirante --; //DECREMENTANDO QUANTIDADE DE CADEIRA -1 (RESTANTES)
+//	    	printf("assentos restantes cadeirantes %d\n", assento_cadeirante);
 		}else{
 			
 			if (participante.tipo_participante == 1){
 				assento_professor -- ;
-				printf("assentos restantes professor %d\n", assento_professor);
+//				printf("assentos restantes professor %d\n", assento_professor);
 			}	
 			else if( participante.tipo_participante == 2 ){
 				assento_aluno --;
-				printf("assentos restantes aluno %d\n", assento_aluno);
+//				printf("assentos restantes aluno %d\n", assento_aluno);
 			}
 		}
-		system("pause");
+//		system("pause"); // SERVE APENAS PARA PARAR O PROGRAMA MOMENTANEAMENTE
 		menu(); // VOLTAR PARA O MENU
 	}
 	
@@ -204,9 +186,35 @@
 		system("PAUSE");
 		menu();
 	}
-	void verificar_vagas(){
-		if(assento_professor >0 && participante.tipo_participante == 1 ){
+       
+	   // ESCOLHA DE ASSENTO
+	void selecionar_assento(){ 
 		
-		}
+		printf("\nSelecione o assento. Exemplo:'A 2'\n"); // 
+		fflush(stdin);
+		scanf("%[^\n]s",participante.escolha_de_assento);// LÊ ATE O USUARIO SELECIONAR O ENTER
+	//	printf("\nescolha_de_assento: %s", escolha_de_assento);
+	    
+		int escolha_fileira = participante.escolha_de_assento[0] - 65;// pegado a escolha da fileira. 65 na tabela ascii = A
+//		printf("escolha de fileira: %d\n", escolha_fileira);
+	    fflush(stdin);
+		
+		int escolha_coluna;
+		sscanf(participante.escolha_de_assento, "%*[^0-9]%d", &escolha_coluna); // pegando somente o inteiro do conteúdo do endereço da variavel de escolha de assento
+		escolha_coluna --; 
+//		printf("escolha de coluna: %d\n", escolha_coluna); 
+		
+		if(participante.tipo_participante == 1 && escolha_fileira > 7  ){// CASO O TIPO DE PARIICIPANTE SEJA PROFESSOR (1)  E SUA ESCOLHA SEJA I OU j
+			assentos[ escolha_fileira][escolha_coluna] = 'x';
+		} 
+		else if (participante.tipo_participante == 2 && escolha_fileira <= 7){
+			assentos[ escolha_fileira][escolha_coluna] = 'x';
+		}else{ 
+			printf(" voce não pode escolher essa fileira\n");
+//			printf(" escolha de fileira: %d\n",escolha_fileira);
+//			printf(" tipo de participante: %d\n",participante.tipo_participante);
+//			
+			selecionar_assento();//chamar função seleção de assento novamente 
+		}		
+	} 
 	
-	}
